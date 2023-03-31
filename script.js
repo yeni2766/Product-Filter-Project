9104c313ced214e4d539b9c7648538730330d908
yeni2766 <yeni.olafeyide@xandertalent.com>
2023-03-31 12:55:57 +0100

    i have added the starter files

---
+++ script.js
@@ -0,0 +1,27 @@
+// Display list of products
+// Argument: An array of products
+function displayProducts(products) {}
+
+// Create a category button
+// Argument: The category, represented in string format.
+function createBtn(category) {}
+
+// Handle input
+// Argument: a string representation of a category, used to compare against categories of products
+function filterProducts(value) {}
+
+// Grab element from the DOM
+// Argument: a string representation of the element's ID
+function getElement(identifier) {}
+
+// Display category buttons
+// Argument: An array of products
+function displayCategories(products) {}
+
+// Gather all categories from products data
+// Argument: An array of products
+function parseCategories(products) {}
+
+// Display initial products list and category buttons
+// No arguments
+function init() {}
